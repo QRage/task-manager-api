02d980bba7c2ab4d3fa580f89824757598df49f2
QRage <sergiobamper@gmail.com>
2025-07-26 09:19:09 +0300

    Revert "feat: Implement distinct PUT and PATCH task update logic"
    This reverts commit 879449ec0f359fdbc8ec32ca49f016b3843b6373.

--- models.py
+++ models.py
@@ -14,16 +14,11 @@ class TaskBase(BaseModel):
 class TaskCreate(TaskBase):
     pass
 
-class TaskPut(BaseModel):
+
+class TaskUpdate(BaseModel):
     title: str = Field(..., min_length=1, max_length=100)
     description: Optional[str] = None
     completed: bool = False
-
-
-class TaskPatch(BaseModel):
-    title: Optional[str] = Field(None, min_length=1, max_length=100)
-    description: Optional[str] = Field(None)
-    completed: Optional[bool] = None
 
 
 class TaskInDB(TaskBase):
